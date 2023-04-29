bf5a062b838f29350a622f817abe1a61ce09c4b2
magic9701 <magic9701@gmail.com>
2023-04-29 10:24:52 +0800

    feat: add StepOne in checkout and base.scss

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import "components/styles/base.scss";
 import StepProgress from "components/main/checkout/StepProgress";
+import StepOne from "components/main/checkout/StepOne";
 
 function App() {
   return (
     <div>
       <StepProgress />
+      <StepOne />
     </div>
   );
 }
--- src/components/main/checkout/StepProgress.js
+++ src/components/main/checkout/StepProgress.js
@@ -6,7 +6,11 @@ function Progress({ dataPhase, number, label }) {
     <span className={styles.progressGroup} data-phase={dataPhase}>
       <span className={styles.progressIcon}>
         <span className={styles.text}>{number}</span>
-        <img className={styles.icon} src={stepUndone} alt="" />
+        <img
+          className={styles.icon + " cursor-point"}
+          src={stepUndone}
+          alt=""
+        />
       </span>
       <span className={styles.progressLabel}>{label}</span>
     </span>
@@ -17,9 +21,9 @@ function StepProgress() {
   return (
     <div>
       {/* register-title */}
-      <h2 className={styles.registerTitle}>結帳</h2>
+      <h2 className={styles.registerTitle + " col col-12"}>結帳</h2>
       {/* register-progress */}
-      <section className={styles.progressContainer}>
+      <section className={styles.progressContainer + " col col-12"}>
         <Progress dataPhase={"address"} number={1} label={"寄送地址"} />
         <span className={styles.progressBar} data-order={1} />
         <Progress dataPhase={"shipping"} number={2} label={"運送方式"} />
