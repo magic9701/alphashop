38b0c0d239c88c04243b1506e50214d043a503d0
magic9701 <magic9701@gmail.com>
2023-04-29 20:20:36 +0800

    feat: add StepTwo in checkout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "components/styles/base.scss";
 import StepProgress from "components/main/checkout/StepProgress";
 import StepOne from "components/main/checkout/StepOne";
+import StepTwo from "components/main/checkout/StepTwo";
 
 function App() {
   return (
     <div>
       <StepProgress />
-      <StepOne />
+      {/* <StepOne /> */}
+      <StepTwo />
     </div>
   );
 }
---
+++ src/components/main/checkout/StepTwo.js
@@ -0,0 +1,41 @@
+import styles from "components/main/checkout/StepTwo.module.scss";
+
+function ShippingWay({ dataPrice, inputId, text, period }) {
+  return (
+    <label className={`${styles.radioGroup} col col-10`} data-price={dataPrice}>
+      <input id={inputId} type="radio" name="shipping" defaultChecked="" />
+      <div className={styles.radioInfo}>
+        <div className="col col-12">
+          <div className={styles.text}>{text}</div>
+          <div className="price" />
+        </div>
+        <div className={`${styles.period} col col-12`}>{period}</div>
+      </div>
+      <div className="radio-box-border" />
+    </label>
+  );
+}
+
+function StepTwo() {
+  return (
+    <form className="col col-12" data-phase="shipping">
+      <h3 className={styles.formTitle}>運送方式</h3>
+      <section className={`${styles.formBody} col col-12`}>
+        <ShippingWay
+          dataPrice={"0"}
+          inputId={"shipping-standard"}
+          text={"標準運送"}
+          period={"約 3~7 個工作天"}
+        />
+        <ShippingWay
+          dataPrice={"500"}
+          inputId={"shipping-dhl"}
+          text={"DHL 貨運"}
+          period={"48 小時內送達"}
+        />
+      </section>
+    </form>
+  );
+}
+
+export default StepTwo;
